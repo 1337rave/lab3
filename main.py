e135ec7be935008b3e494e30416e91e55a2152cb
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2023-09-25 17:30:12 +0300

    ex 4 was done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-try:
-    pass
-except Exception as (e):
-    print(e)
+number1 = float(input("Введіть перше число: "))
+number2 = float(input("Введіть друге число: "))
+minimum = min(number1, number2)
+print(f"Мінімум із {number1} і {number2} - це {minimum}.")
