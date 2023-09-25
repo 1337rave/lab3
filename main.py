c25c270aa737015c63f516bf7f1f4ae06c728553
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2023-09-25 17:31:35 +0300

    ex 5 was done

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-try:
-    pass
-except Exception as (e):
-    print(e)
+operation = input("Виберіть операцію (+ для додавання, - для віднімання, * для множення): ")
+number1 = float(input("Введіть перше число: "))
+number2 = float(input("Введіть друге число: "))
+if operation == '+':
+    result = number1 + number2
+    print(f"Сума {number1} і {number2} дорівнює {result}")
+elif operation == '-':
+    result = number1 - number2
+    print(f"Різниця {number1} і {number2} дорівнює {result}")
+elif operation == '*':
+    result = number1 * number2
+    print(f"Добуток {number1} і {number2} дорівнює {result}")
+else:
+    print("Невідома операція. Будь ласка, виберіть +, - або *.")
+
