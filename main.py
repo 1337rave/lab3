b57547939f0ab42808f78dda266fb8330604aaf3
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2023-09-25 17:29:40 +0300

    ex 3 was done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-try:
-    pass
-except Exception as (e):
-    print(e)
+number1 = float(input("Введіть перше число: "))
+number2 = float(input("Введіть друге число: "))
+maximum = max(number1, number2)
+print(f"Максимум із {number1} і {number2} - це {maximum}.")
